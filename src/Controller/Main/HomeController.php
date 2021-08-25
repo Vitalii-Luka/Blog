3e148a18ec067e6c47b2a20a56d89df6b351f27f
Vitalii Luka <luka.vitalii.ck@gmail.com>
2021-08-25 16:37:48 +0300

    Creating a HomePage: added Controller and template

---
+++ src/Controller/Main/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function __invoke(): Response
+    {
+        return $this->render('main/home/index.html.twig');
+    }
+}
